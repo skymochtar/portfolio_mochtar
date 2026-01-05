8bc22d7dddcaa78b3d325f7bdba60f1717ddbcdf
Mochtario Dwi Wahyudi <mochtariodw@gmail.com>
2026-01-06 00:21:04 +0700

    force upload generated localization files

---
+++ lib/src/localization/generated/locale_json.g.dart
@@ -0,0 +1,509 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes, avoid_renaming_method_parameters, constant_identifier_names
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class CodegenLoader extends AssetLoader{
+  const CodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>?> load(String path, Locale locale) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String,dynamic> _en = {
+  "name": "Mochtario Dwi Wahyudi",
+  "description": "Mobile App Developer",
+  "subDescription": "Flutter Enthusiast",
+  "contacts": [
+    {
+      "tooltip": "Github",
+      "url": "https://github.com/skymochtar",
+      "icon": {
+        "codePoint": "0xefb7",
+        "fontFamily": "FontAwesome"
+      }
+    },
+    {
+      "tooltip": "Instagram",
+      "url": "https://instagram.com/SkyMochtar8",
+      "icon": {
+        "assetName": "assets/icons/software-development/instagram.svg",
+        "color": "0xffffffff"
+      }
+    },
+    {
+      "tooltip": "Twitter",
+      "url": "https://twitter.com/",
+      "icon": {
+        "codePoint": "0xf0ea",
+        "fontFamily": "FontAwesome"
+      }
+    },
+    {
+      "tooltip": "LinkedIn",
+      "url": "https://www.linkedin.com/in/mochtario-dwi-wahyudi",
+      "icon": {
+        "codePoint": "0xeffb",
+        "fontFamily": "FontAwesome"
+      }
+    },
+    {
+      "tooltip": "Email",
+      "url": "mailto:mochtariodw@gmail.com",
+      "icon": {
+        "codePoint": "0xeaad",
+        "fontFamily": "FontAwesome"
+      }
+    },
+    {
+      "tooltip": "+62 851 6994 7561",
+      "url": "tel:+6285169947561",
+      "icon": {
+        "codePoint": "0xecbb",
+        "fontFamily": "FontAwesome"
+      }
+    }
+  ],
+  "resumes": [
+    {
+      "languageCode": "en",
+      "url": "https://drive.google.com/file/d/1jFxPDC3q8LTSwEyN1gyw2uZReR4cW8p2/view?usp=sharing"
+    },
+    {
+      "languageCode": "id",
+      "url": "https://drive.google.com/file/d/1SEDu-wdXXsYXX48Ee3EKLg7YhdKyt72U/view?usp=sharing"
+    }
+  ],
+  "aboutDescription": "I'm a mobile app developer passionate about turning ideas into user-centric applications.\n\nWith experience in Flutter and Web Development, I enjoy solving complex problems and building meaningful digital products. I am constantly learning new technologies to improve my skills and deliver high-quality code.",
+  "experiences": [
+    {
+      "role": "Web Developer & UI/UX Intern",
+      "company": "PT Vinix Seven Aurum",
+      "description": "Collaborated with a team to design and build responsive web applications. Translated UI/UX designs from Figma into clean HTML/CSS/JS code and ensured cross-browser compatibility.",
+      "isPresent": false,
+      "startYear": 2025,
+      "startMonth": 9,
+      "endYear": 2025,
+      "endMonth": 12,
+      "technologies": [
+        {
+          "name": "HTML",
+          "icon": {
+            "assetName": "assets/icons/software-development/html-5.svg"
+          }
+        },
+        {
+          "name": "CSS",
+          "icon": {
+            "assetName": "assets/icons/software-development/css3.svg"
+          }
+        },
+        {
+          "name": "Figma",
+          "icon": {
+            "assetName": "assets/icons/software-development/figma.svg"
+          }
+        }
+      ],
+      "links": [
+        {
+          "url": "https://drive.google.com/file/d/1l-eMLUYL894YdiCeKlaQwb0oJP70FL4O/view?usp=sharing",
+          "display": "Certificate"
+        }
+      ]
+    }
+  ],
+  "present": "Present",
+  "projects": [
+    {
+      "name": "Portfolio",
+      "description": "A fully responsive personal portfolio website built with Flutter Web. It features a modern UI/UX design, dark/light theme support, and multi-language localization. Implemented using Riverpod for efficient state management.",
+      "url": "https://github.com/skymochtar/portfolio_mochtar#readme",
+      "icon": {
+        "codePoint": "0xefb7",
+        "fontFamily": "FontAwesome",
+        "color": "0xffffffff"
+      },
+      "screenshotPath": "assets/images/portfolio.png",
+      "technologies": [
+        {
+          "name": "Flutter",
+          "icon": {
+            "assetName": "assets/icons/software-development/flutter.svg"
+          }
+        },
+        {
+          "name": "Dart",
+          "icon": {
+            "assetName": "assets/icons/other/dart.svg"
+          }
+        }
+      ],
+      "links": [
+        {
+          "url": "https://portfolio-mochtar.vercel.app/",
+          "display": "View Website"
+        }
+      ]
+    },
+    {
+      "name": "WasteScan Sky AI",
+      "description": "A smart waste classification mobile app built with Flutter. It utilizes TensorFlow Lite (TFLite) for real-time object detection via the camera, helping users identify waste types efficiently. Features clean architecture and Riverpod for state management.",
+      "url": "https://github.com/skymochtar/WasteScan-Sky-AI#readme",
+      "icon": {
+        "codePoint": "0xefb7",
+        "fontFamily": "FontAwesome",
+        "color": "0xffffffff"
+      },
+      "screenshotPath": "assets/images/wastescan.png",
+      "technologies": [
+        {
+          "name": "Flutter",
+          "icon": {
+            "assetName": "assets/icons/software-development/flutter.svg"
+          }
+        },
+        {
+          "name": "Dart",
+          "icon": {
+            "assetName": "assets/icons/other/dart.svg"
+          }
+        }
+      ]
+    },
+    {
+      "name": "Palindrome & User List",
+      "description": "A mobile technical test application. Features include a logic algorithm for Palindrome checking and a paginated user list integrated with REST API (Reqres.in). Built with a focus on clean code and interactive UI.",
+      "url": "https://github.com/skymochtar/suitmedia-test-flutter#readme",
+      "icon": {
+        "codePoint": "0xf0ca",
+        "fontFamily": "FontAwesome",
+        "color": "0xffffffff"
+      },
+      "screenshotPath": "assets/images/desain.png",
+      "technologies": [
+        {
+          "name": "Flutter",
+          "icon": {
+            "assetName": "assets/icons/software-development/flutter.svg"
+          }
+        },
+        {
+          "name": "Dart",
+          "icon": {
+            "assetName": "assets/icons/other/dart.svg"
+          }
+        },
+        {
+          "name": "REST API",
+          "icon": {
+            "codePoint": "0xf0c1",
+            "fontFamily": "FontAwesome"
+          }
+        }
+      ]
+    },
+    {
+      "name": "Professional Certifications",
+      "description": "Documentation of my professional expertise certificates. Includes competency certifications from Dicoding Indonesia, Codepolitan, BNSP, Cisco, Alibaba, Coursera, and Google Career Certificates.",
+      "url": "https://drive.google.com/file/d/1tG3AxYjrUDmDryMOORmL0pwxzrF9442i/view?usp=sharing/",
+      "icon": {
+        "codePoint": "0xf0a3",
+        "fontFamily": "FontAwesome",
+        "color": "0xffffffff"
+      },
+      "screenshotPath": "assets/images/certificates_collage.png",
+      "links": [
+        {
+          "url": "https://drive.google.com/drive/folders/1RZ_UmlIBUqVmCdORPK5i8ZWbNgjsVPri?usp=sharing",
+          "display": "View Certificates (Drive)"
+        }
+      ]
+    }
+  ],
+  "languages": [
+    {
+      "code": "en",
+      "name": "English",
+      "nativeName": "English",
+      "icon": {
+        "assetName": "assets/icons/flags/united-states-of-america.svg"
+      }
+    },
+    {
+      "code": "id",
+      "name": "Indonesian",
+      "nativeName": "Bahasa Indonesia",
+      "icon": {
+        "assetName": "assets/icons/flags/indonesia.svg"
+      }
+    }
+  ],
+  "bottomBanner": {
+    "message": "Portfolio -",
+    "displayLink": "SkyMochtar",
+    "linkUrl": "https://github.com/skymochtar/portfolio_mochtar#readme"
+  },
+  "portfolio": "Portfolio",
+  "homeSectionTitle": "Home",
+  "aboutSectionTitle": "About",
+  "aboutSectionTitleAlt": "About Me",
+  "experienceSectionTitle": "Experience",
+  "projectsSectionTitle": "Projects",
+  "resume": "Resume",
+  "downloadResume": "Download resume",
+  "openUrlError": "Could not open the url",
+  "unknownLanguageError": "Language unknown"
+};
+static const Map<String,dynamic> _id = {
+  "name": "Mochtario Dwi Wahyudi",
+  "description": "Pengembang Aplikasi Mobile",
+  "subDescription": "Antusias Flutter",
+  "contacts": [
+    {
+      "tooltip": "Github",
+      "url": "https://github.com/skymochtar",
+      "icon": {
+        "codePoint": "0xefb7",
+        "fontFamily": "FontAwesome"
+      }
+    },
+    {
+      "tooltip": "Instagram",
+      "url": "https://instagram.com/SkyMochtar8",
+      "icon": {
+        "assetName": "assets/icons/software-development/instagram.svg",
+        "color": "0xffffffff"
+      }
+    },
+    {
+      "tooltip": "Twitter",
+      "url": "https://twitter.com/",
+      "icon": {
+        "codePoint": "0xf0ea",
+        "fontFamily": "FontAwesome"
+      }
+    },
+    {
+      "tooltip": "LinkedIn",
+      "url": "https://www.linkedin.com/in/mochtario-dwi-wahyudi",
+      "icon": {
+        "codePoint": "0xeffb",
+        "fontFamily": "FontAwesome"
+      }
+    },
+    {
+      "tooltip": "Email",
+      "url": "mailto:mochtariodw@gmail.com",
+      "icon": {
+        "codePoint": "0xeaad",
+        "fontFamily": "FontAwesome"
+      }
+    },
+    {
+      "tooltip": "+62 851 6994 7561",
+      "url": "tel:+6285169947561",
+      "icon": {
+        "codePoint": "0xecbb",
+        "fontFamily": "FontAwesome"
+      }
+    }
+  ],
+  "resumes": [
+    {
+      "languageCode": "en",
+      "url": "https://drive.google.com/file/d/1jFxPDC3q8LTSwEyN1gyw2uZReR4cW8p2/view?usp=sharing"
+    },
+    {
+      "languageCode": "id",
+      "url": "https://drive.google.com/file/d/1SEDu-wdXXsYXX48Ee3EKLg7YhdKyt72U/view?usp=sharing"
+    }
+  ],
+  "aboutDescription": "Saya adalah pengembang aplikasi mobile yang bersemangat mengubah ide menjadi aplikasi yang berpusat pada pengguna.\n\nDengan pengalaman di Flutter dan Pengembangan Web, saya menikmati memecahkan masalah kompleks dan membangun produk digital yang bermanfaat. Saya terus mempelajari teknologi baru untuk meningkatkan keterampilan dan menghasilkan kode yang berkualitas tinggi.",
+  "experiences": [
+    {
+      "role": "Web Developer & UI/UX Intern",
+      "company": "PT Vinix Seven Aurum",
+      "description": "Berkolaborasi dengan tim untuk merancang dan membangun aplikasi web yang responsif. Menerjemahkan desain UI/UX dari Figma menjadi kode HTML/CSS/JS yang rapi serta memastikan kompatibilitas lintas browser.",
+      "isPresent": false,
+      "startYear": 2025,
+      "startMonth": 9,
+      "endYear": 2025,
+      "endMonth": 12,
+      "technologies": [
+        {
+          "name": "HTML",
+          "icon": {
+            "assetName": "assets/icons/software-development/html-5.svg"
+          }
+        },
+        {
+          "name": "CSS",
+          "icon": {
+            "assetName": "assets/icons/software-development/css3.svg"
+          }
+        },
+        {
+          "name": "Figma",
+          "icon": {
+            "assetName": "assets/icons/software-developmnent/figma.svg"
+          }
+        }
+      ],
+      "links": [
+        {
+          "url": "https://drive.google.com/file/d/1l-eMLUYL894YdiCeKlaQwb0oJP70FL4O/view?usp=sharing",
+          "display": "Sertifikat"
+        }
+      ]
+    }
+  ],
+  "present": "Sekarang",
+  "projects": [
+    {
+      "name": "Portfolio",
+      "description": "Website portofolio pribadi yang sepenuhnya responsif, dibangun dengan Flutter Web. Memiliki desain UI/UX modern, dukungan tema gelap/terang, dan multi-bahasa. Diimplementasikan menggunakan Riverpod untuk manajemen state yang efisien.",
+      "url": "https://github.com/skymochtar/portfolio_mochtar#readme",
+      "icon": {
+        "codePoint": "0xefb7",
+        "fontFamily": "FontAwesome",
+        "color": "0xffffffff"
+      },
+      "screenshotPath": "assets/images/portfolio.png",
+      "technologies": [
+        {
+          "name": "Flutter",
+          "icon": {
+            "assetName": "assets/icons/software-development/flutter.svg"
+          }
+        },
+        {
+          "name": "Dart",
+          "icon": {
+            "assetName": "assets/icons/other/dart.svg"
+          }
+        }
+      ],
+      "links": [
+        {
+          "url": "https://portfolio-mochtar.vercel.app/",
+          "display": "Lihat Website"
+        }
+      ]
+    },
+    {
+      "name": "WasteScan Sky AI",
+      "description": "Aplikasi mobile klasifikasi sampah cerdas yang dibangun dengan Flutter. Menggunakan TensorFlow Lite (TFLite) untuk deteksi objek secara real-time melalui kamera, membantu pengguna mengidentifikasi jenis sampah secara efisien. Menggunakan clean architecture dan Riverpod.",
+      "url": "https://github.com/skymochtar/WasteScan-Sky-AI#readme",
+      "icon": {
+        "codePoint": "0xefb7",
+        "fontFamily": "FontAwesome",
+        "color": "0xffffffff"
+      },
+      "screenshotPath": "assets/images/wastescan.png",
+      "technologies": [
+        {
+          "name": "Flutter",
+          "icon": {
+            "assetName": "assets/icons/software-development/flutter.svg"
+          }
+        },
+        {
+          "name": "Dart",
+          "icon": {
+            "assetName": "assets/icons/other/dart.svg"
+          }
+        }
+      ]
+    },
+    {
+      "name": "Palindrome & User List",
+      "description": "Aplikasi tes teknis mobile. Memiliki fitur algoritma logika pengecekan Palindrome dan daftar pengguna dengan pagination yang terintegrasi dengan REST API (Reqres.in). Dibangun dengan fokus pada clean code dan UI yang interaktif.",
+      "url": "https://github.com/skymochtar/suitmedia-test-flutter#readme",
+      "icon": {
+        "codePoint": "0xf0ca",
+        "fontFamily": "FontAwesome",
+        "color": "0xffffffff"
+      },
+      "screenshotPath": "assets/images/desain.png",
+      "technologies": [
+        {
+          "name": "Flutter",
+          "icon": {
+            "assetName": "assets/icons/software-development/flutter.svg"
+          }
+        },
+        {
+          "name": "Dart",
+          "icon": {
+            "assetName": "assets/icons/other/dart.svg"
+          }
+        },
+        {
+          "name": "REST API",
+          "icon": {
+            "codePoint": "0xf0c1",
+            "fontFamily": "FontAwesome"
+          }
+        }
+      ]
+    },
+    {
+      "name": "Koleksi Sertifikasi Kompetensi",
+      "description": "Dokumentasi sertifikat keahlian profesional saya. Termasuk sertifikasi kompetensi dari Dicoding Indonesia, Codepolitan, BNSP, Cisco, Alibaba, Coursera, dan Google Career Certificates.",
+      "url": "https://drive.google.com/file/d/1tG3AxYjrUDmDryMOORmL0pwxzrF9442i/view?usp=sharing/",
+      "icon": {
+        "codePoint": "0xf0a3",
+        "fontFamily": "FontAwesome",
+        "color": "0xffffffff"
+      },
+      "screenshotPath": "assets/images/certificates_collage.png",
+      "technologies": [],
+      "links": [
+        {
+          "url": "https://drive.google.com/drive/folders/1RZ_UmlIBUqVmCdORPK5i8ZWbNgjsVPri?usp=sharing",
+          "display": "Lihat Sertifikat (Drive)"
+        }
+      ]
+    }
+  ],
+  "languages": [
+    {
+      "code": "en",
+      "name": "English",
+      "nativeName": "English",
+      "icon": {
+        "assetName": "assets/icons/flags/united-states-of-america.svg"
+      }
+    },
+    {
+      "code": "id",
+      "name": "Indonesian",
+      "nativeName": "Bahasa Indonesia",
+      "icon": {
+        "assetName": "assets/icons/flags/indonesia.svg"
+      }
+    }
+  ],
+  "bottomBanner": {
+    "message": "Portofolio -",
+    "displayLink": "SkyMochtar",
+    "linkUrl": "https://github.com/skymochtar/portfolio_mochtar#readme"
+  },
+  "portfolio": "Portofolio",
+  "homeSectionTitle": "Beranda",
+  "aboutSectionTitle": "Tentang",
+  "aboutSectionTitleAlt": "Tentang Saya",
+  "experienceSectionTitle": "Pengalaman",
+  "projectsSectionTitle": "Proyek",
+  "resume": "Resume",
+  "downloadResume": "Unduh Resume",
+  "openUrlError": "Tidak dapat membuka url",
+  "unknownLanguageError": "Bahasa tidak dikenal"
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"en": _en, "id": _id};
+}
